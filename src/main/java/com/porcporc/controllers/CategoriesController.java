3761e47dd6951a73b513a487ec8f0194a5a24970
Zamfir Catalin <catalin_provideo@yahoo.com>
2020-12-17 11:00:09 +0200

    finish with categories

--- src/main/java/com/porcporc/controllers/CategoriesController.java
+++ src/main/java/com/porcporc/controllers/CategoriesController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @Controller
 public class CategoriesController {
 
     private static Logger logger = LoggerFactory.getLogger(CategoriesController.class);
 
-    public CategoriesController(){
+    public CategoriesController() {
         logger.info(getClass().getSimpleName() + " created");
     }
 
@@ -26,7 +28,7 @@ public class CategoriesController {
     private CategoriesRepository categoriesRepository;
 
     @GetMapping("/categories")
-    public ModelAndView getCategories(){
+    public ModelAndView getCategories() {
         ModelAndView modelAndView = new ModelAndView("categories");
         modelAndView.addObject("categoriesList", categoriesRepository.findAll());
         return modelAndView;
@@ -39,29 +41,30 @@ public class CategoriesController {
         return modelAndView;
     }
 
+
     @GetMapping("categories/edit/{id}")
     public ModelAndView editCategories(@PathVariable Integer id){
         ModelAndView modelAndView = new ModelAndView("categories-formular");
-        modelAndView.addObject("categories", categoriesRepository.findById(id).get());
+        CategoriesEntity categoriesEntity = categoriesRepository.findById(id).get();
+        modelAndView.addObject("categories", categoriesEntity);
         return modelAndView;
-    }
-
+}
 
     @GetMapping("/categories/delete/{id}")
     public ModelAndView deleteCategories(@PathVariable Integer id){
-        ModelAndView modelAndView = new ModelAndView("categories-formular");
+        ModelAndView modelAndView = new ModelAndView("redirect:/categories");
         categoriesRepository.deleteById(id);
         return modelAndView;
     }
 
 
-
-
     @PostMapping("categories/save")
-    public ModelAndView saveCategories(@ModelAttribute ProductsEntity product){
+    public ModelAndView saveCategories(@ModelAttribute CategoriesEntity categories){
         ModelAndView modelAndView = new ModelAndView("redirect:/categories");
         modelAndView.setViewName("categories-formular");
-        modelAndView.addObject("categories", product);
+        modelAndView.addObject("categories", categories);
+        categoriesRepository.save(categories);
+        modelAndView.setViewName("redirect:/categories");
         return modelAndView;
     }
 
